69d5b05aa2e7096df5af1717bda0d010b92bddbd
Mr. YASEEN <elmyaseen5@gmail.com>
2023-07-23 19:31:04 +0530

    th

--- twentyss/src/components/home/ProductList.vue
+++ twentyss/src/components/home/ProductList.vue
@@ -58,7 +58,7 @@ export default {
     </div>
   </div>
 
-  <Banner v-for="i in banner" :key="i.id" :bannerImage="banner"/>
+  <Banner v-for="bannerImage in banner" :key="bannerImage.id" :bannerImage="banner"/>
 
   <div class="row pt-3">
       <div class="col-md-6 col-7">
